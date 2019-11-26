43b8b8bb2a36fe43272752b3ada66776fea50d59
Brandon <bchen00@stuy.edu>
2019-11-26 10:58:30 -0500

    wrote parse_args but havent test it

--- strsepArgs.c
+++ strsepArgs.c
@@ -2,9 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+char ** parse_args( char * line ){
+  char ** var[6];
+  int size;
+  for (size = 0; size < 5; size++){
+    var[size] = strsep(&line, " ");
+  }
+  return var;
+}
+
 int main(){
+  /**
   char line[100] = "woah-this-is-cool";
   char *s1 = line;
   printf("[%s]\n", strsep( &s1, "-" ));
   printf("[%s]\n", s1);
+  printf("[%s]\n", strsep( &s1, "-" ));
+  printf("[%s]\n", s1);
+  printf("[%s]\n", strsep( &s1, "-" ));
+  printf("[%s]\n", s1);
+  printf("[%s]\n", strsep( &s1, "-" ));
+  printf("[%s]\n", s1);
+  **/
 }
